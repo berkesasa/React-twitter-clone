7184a7d49223968e1338b7e8267cfc1890d74c12
berkesasa <berkesasa11@gmail.com>
2024-01-02 20:09:01 +0300

    created rightbar premium

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -4,9 +4,9 @@ import PropTypes from "prop-types"
 
 export default function Button({ size, children }) {
     return createElement('button', {
-        className: classNames("bg-[#1d9bf0] rounded-full flex items-center font-bold hover:bg-[#1a8cd8] transition-colors w-[90%] justify-center",{
+        className: classNames("bg-[#1d9bf0] rounded-full text-white flex items-center font-bold hover:bg-[#1a8cd8] transition-colors justify-center",{
             "px-4 h-9": size == 'normal',
-            "px-4 text-[17px] h-[52px]": size == 'large',
+            "px-4 text-[17px] h-[52px] w-full": size == 'large',
         })
     }, children)
 }
